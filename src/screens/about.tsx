b3a7197155f314ff8fb2080031fc6b78adeb5fd8
parav101 <90014445+parav101@users.noreply.github.com>
2024-05-26 13:03:41 +0530

    updated about and contact page

--- src/screens/about.tsx
+++ src/screens/about.tsx
@@ -7,13 +7,10 @@ function About() {
         <div className="container mx-auto py-16 px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
             <div className="max-w-lg">
-              <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">About Us</h2>
-              <p className="mt-4 text-gray-600 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis eros at lacus feugiat hendrerit sed ut tortor. Suspendisse et magna quis elit efficitur consequat. Mauris eleifend velit a pretium iaculis. Donec sagittis velit et magna euismod, vel aliquet nulla malesuada. Nunc pharetra massa lectus, a fermentum arcu volutpat vel.</p>
+              <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">About Me</h2>
+              <p className="mt-4 text-gray-600 text-lg">Hi my name is Paravdeep Singh, I'm detail-oriented and motivated new developer. Eager to apply my knowledge and contribute to dynamic projects while continuously expanding my skill set. Seeking an opportunity to work with a collaborative team in a growth-oriented environment to develop innovative solutions that drive success for both the company and its clients.</p>
               <div className="mt-8">
-                <a href="#" className="text-blue-500 hover:text-blue-600 font-medium">
-                  Learn more about us
-                  <span className="ml-2">&#8594;</span>
-                </a>
+               
               </div>
             </div>
             <div className="mt-12 md:mt-0">

eb17c7250fa020c271db01dba5478d0ddc1771bb
parav101 <90014445+parav101@users.noreply.github.com>
2024-05-26 17:15:00 +0530

    changed accountInfo

--- src/screens/accountInfo.tsx
+++ src/screens/accountInfo.tsx
@@ -28,17 +28,7 @@ function AccountInfo() {
               <dt className="text-sm font-medium text-gray-500">Email address</dt>
               <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{user.email}</dd>
             </div>
-            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm font-medium text-gray-500">Phone number</dt>
-              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">check <span className='text-blue-600 cursor-pointer hover:underline' onClick={()=>navigate('/account-address')}>addresses</span></dd>
-            </div>
-            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm font-medium text-gray-500">Default Address</dt>
-              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                use addresss to set default <span className='text-blue-600 cursor-pointer hover:underline' onClick={()=>navigate('/account-address')}>addresses</span>
-                
-              </dd>
-            </div>
+          
           </dl>
         </div>
       </div>
